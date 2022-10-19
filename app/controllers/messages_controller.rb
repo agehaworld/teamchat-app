036d212947242a30601efdee76ceb8d28ccfd36d
agehaworld <kazuyogroenewold@gmail.com>
2022-10-19 09:09:00 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,22 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @team = Team.find(params[:team_id])
+  end
+
+  def create
+    @team = Team.find(params[:team_id])
+    @message = @team.messages.new(message_params)
+    if @message.save
+      redirect_to team_messages_path(@team)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
